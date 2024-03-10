7d8e008b827b5dfa376e558fb9f5916b6f8fdf3a
mahmoud <ramadanmahmoud945@gmail.com>
2024-03-10 03:28:52 +0200

    aswzx

--- my_printf.c
+++ my_printf.c
@@ -18,47 +18,56 @@ va_start(p, format);
 while (format[x] != '\0')
 {
 	if (format[x] == '%')
-	{
-		if (format[x + 1] == 'c')
+	{x++;
+		if (format[x] == 'c')
 		{
 			ch = va_arg(p, int);
 			_putchar(ch);
-			
-			if ((format[x + 2]) == '\0')
+			q++;
+			i--;
+			if ((format[x + 1]) == '\0')
 			{
-				i++;
 				/*printf("i--(%d)--\n", i);*/
 				break;
 			}
-			q++;
-			x++;
+			
 		}
-		else if (format[x + 1] == 's')
+		else if (format[x] == 's')
 		{
 			s = va_arg(p, char *);
-			k = roo(s);
-			_putchar('\0');
-		/*write(1, s, (1 + strlen(s)));*/
-			if ((format[x + 2]) == '\0')
+			if (s == NULL)
 			{
-				i = i + k;
+				k = roo("(null)");
+				r = r + k;
+				i--;
+			}
+			else
+			{
+			k = roo(s);
+			/*printf("k--(%d)--\n", k);*/
+			_putchar('\0');
+			r = r + k;
+			i--;
+			}
+		/*write(1, s, (1 + strlen(s)));*/
+			if ((format[x + 1]) == '\0')
+			{
 				break;
 			}
-			r = k;
-			x++;
+			i--;
 		}
-		x++;
 	}
-
+	else
 _putchar(format[x]);
-/*write(1, &format[x], 1);*/
-
-x++;
 i++;
+/*write(1, &format[x], 1);*/
+x++;
+/*printf("--(%d)--\n", i);*/
 }
 
 
 va_end(p);
-/*printf("(i)--%d--(r)--%d--(q)--%d\n", i, r, q);*/
+/*printf("(i)--%d--(r)--%d--(q)--%d--\n", i, r, q);*/
+i++;
 return (i + r + q);
 }
